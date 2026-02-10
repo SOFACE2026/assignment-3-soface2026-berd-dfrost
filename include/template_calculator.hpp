e097377292949c85adbd7055b514c059a911e3b7
Daniel Frost Svenningsen <202505828@post.au.dk>
2026-02-10 19:08:23 +0100

    opgave 3

--- include/template_calculator.hpp
+++ include/template_calculator.hpp
@@ -11,16 +11,27 @@ public:
     {
     }
 
-    // Implement this:
+    // literally a copypaste from int_calculator.cpp with "int" changed to "T"
     T sum(T a, T b)
     {
-        return 0;
+        T sum = a + b;
+
+        std::ostringstream output;
+        output << "taking the sum of: " << a << " and " << b << " which is " << sum << std::endl;
+        logger->log(output.str());  // send the string to the logger, any type of logger
+        
+        return sum;
     }
 
-    // Implement this:
     T multiply(T a, T b)
     {
-        return 0;
+        T product = a * b;
+        
+        std::ostringstream output;
+        output << "taking the product of: " << a << " and " << b << " which is " << product << std::endl;
+        logger->log(output.str());  // send the string to the logger, any type of logger
+        
+        return product;
     }
 
 private:
